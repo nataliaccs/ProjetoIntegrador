f09aa95a972f804cca4625e747b062b95e8795b9
NatáliaCosta <naty1993ccs@gmail.com>
2022-08-03 14:49:27 -0300

    ModelSubtemas

---
+++ projetointegrador/src/main/java/org/generation/projetointegrador/model/SubtemasModel.java
@@ -0,0 +1,40 @@
+package org.generation.projetointegrador.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name= "tb_subtemas")
+public class SubtemasModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	
+	@NotBlank(message = "O atributo descrição é obrigatório e não pode utilizar espaços em branco!") 
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
+
+
+
