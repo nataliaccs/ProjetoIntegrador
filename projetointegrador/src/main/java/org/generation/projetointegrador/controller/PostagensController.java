30d408df23883d81dde9d5da4a14031dbae1c9df
Rebeka Sena <bekacristine@gmail.com>
2022-08-09 11:55:38 -0300

    Camadas Postagens

---
+++ projetointegrador/src/main/java/org/generation/projetointegrador/controller/PostagensController.java
@@ -0,0 +1,66 @@
+package org.generation.projetointegrador.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.generation.projetointegrador.model.PostagensModel;
+import org.generation.projetointegrador.repository.PostagensRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+
+
+public class PostagensController {
+
+		@Autowired
+		private PostagensRepository postagensRepository;
+		
+		@GetMapping
+		public ResponseEntity<List<PostagensModel>> GetAll(){
+			return ResponseEntity.ok(postagensRepository.findAll());
+		}
+		
+		@GetMapping("/{id}")
+		public ResponseEntity<PostagensModel> GetById(@PathVariable Long id){
+			return postagensRepository.findById(id)
+					.map(resp -> ResponseEntity.ok(resp))
+					.orElse(ResponseEntity.notFound().build());
+		}
+		
+		@GetMapping("/titulo/{titulo}")
+		public ResponseEntity<List<PostagensModel>> GetByTitulo(@PathVariable String titulo){
+			return ResponseEntity.ok(postagensRepository.findAllByTituloContainingIgnoreCase(titulo));		
+		}
+	
+		@PostMapping
+		public ResponseEntity<PostagensModel> postPostagens (@Valid @RequestBody PostagensModel  postagens ){
+			//if (subtemasRepository.existsById(postagens.getSubtemasRepository().getId()))
+				return ResponseEntity.status(HttpStatus.CREATED).body(postagensRepository.save(postagens));
+			//return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+		
+		@PutMapping
+		public ResponseEntity <PostagensModel> putPostagens (@RequestBody PostagensModel postagens){
+			return ResponseEntity.status(HttpStatus.OK).body(postagensRepository.save(postagens));
+		}
+		
+		@DeleteMapping ("/{id}")
+		public void deletePostagens (@PathVariable Long id) {
+			postagensRepository.deleteById(id);
+		}
+}
+
--- projetointegrador/src/main/java/org/generation/projetointegrador/model/SubtemasModel.java
+++ projetointegrador/src/main/java/org/generation/projetointegrador/model/SubtemasModel.java
@@ -1,11 +1,17 @@
 package org.generation.projetointegrador.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name= "tb_subtemas")
@@ -18,6 +24,10 @@ public class SubtemasModel {
 	@NotBlank(message = "O atributo descrição é obrigatório e não pode utilizar espaços em branco!") 
 	private String descricao;
 
+	@OneToMany(mappedBy = "subtemas", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("subtemas")
+    private List<PostagensModel> postagens;
+	
 	public Long getId() {
 		return id;
 	}
@@ -33,6 +43,15 @@ public class SubtemasModel {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
+	public List<PostagensModel> getPostagens() {
+		return postagens;
+	}
+
+	public void setPostagens(List<PostagensModel> postagens) {
+		this.postagens = postagens;
+	}
+	
 
 }
 
