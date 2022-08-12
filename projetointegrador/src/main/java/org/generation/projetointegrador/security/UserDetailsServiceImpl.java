e1194a137a137d7369c30229d3942b125d928309
ElisCavalcante <elisabeth.rsc@outlook.com>
2022-08-12 15:40:33 -0300

    Implementação da segurança

---
+++ projetointegrador/src/main/java/org/generation/projetointegrador/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package org.generation.projetointegrador.security;
+
+import java.util.Optional;
+
+import org.generation.projetointegrador.model.UsuariosModel;
+import org.generation.projetointegrador.repository.UsuariosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.server.ResponseStatusException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UsuariosRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+		Optional<UsuariosModel> usuario = usuarioRepository.findByUsuario(userName);
+
+		if (usuario.isPresent())
+			return new UserDetailsImpl(usuario.get());
+		else
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+	}
+
+}
